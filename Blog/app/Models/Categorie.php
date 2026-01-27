dd002831d74e913d9a726e4d23de13028b1dc243
oughlane <mohammedoughlane72@gmail.com>
2026-01-27 09:15:33 +0100

    creer retrospective

---
+++ Blog/app/Models/Categorie.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+    class Categorie extends Model
+{
+    protected $fillable = ['nom', 'description'];
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+}
+
---
+++ Blog/app/Models/Post.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+    class Post extends Model
+{
+    protected $fillable = [
+        'titre',
+        'contenu',
+        'image',
+        'categorie_id'
+    ];
+}
+
